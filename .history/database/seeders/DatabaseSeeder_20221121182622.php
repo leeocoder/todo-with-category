a54327a1d051d5f62ca0c47f45596879f293d1d5
Leonardo Albuquerque <leonardo@macrolabs.com.br>
2022-12-05 05:47:10 -0300

    feat: make a seeder for user

--- .history/database/seeders/DatabaseSeeder_20221121182622.php
+++ .history/database/seeders/DatabaseSeeder_20221121182622.php
@@ -4,7 +4,6 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Database\Seeders\UserSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,9 +14,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call([
-            UserSeeder::class
-        ]);
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
